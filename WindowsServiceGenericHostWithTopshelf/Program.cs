e062c42ec78ac7097504b56539f755bbd395f6b9
wanglong <nmgimust_wl@126.com>
2019-01-22 15:32:36 +0800

    Net Core 22 Windows Service Generic Host With Topshelf

---
+++ WindowsServiceGenericHostWithTopshelf/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Topshelf;
+
+namespace WindowsServiceGenericHostWithTopshelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Windows Service Generic Host With Topshelf!");
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<IHostLifetime, TopshelfLifetime>();
+                    services.AddHostedService<FileWriterService>();
+                });
+
+            HostFactory.Run(x =>
+            {
+                x.SetServiceName("WindowsServiceGenericHostWithTopshelf");
+                x.SetDisplayName("Topshelf创建的Generic Host服务");
+                x.SetDescription("运行Topshelf创建的Generic Host服务");
+                x.Service<IHost>(s =>
+                {
+                    s.ConstructUsing(() => builder.Build());
+                    s.WhenStarted(service =>
+                    {
+                        service.StartAsync();
+                    });
+                    s.WhenStopped(service =>
+                    {
+                        service.StopAsync();
+                    });
+                });
+            });
+        }
+    }
+}
---
+++ WindowsServiceGenericHostWithTopshelf/ReadMe.txt
@@ -0,0 +1,7 @@
+﻿# GenericHostWindowsServiceWithTopshelf
+a demo for run .NET Core Generic Host as Windows Service using Topshelf
+
+安装：WindowsServiceGenericHostWithTopshelf.exe install
+启动：WindowsServiceGenericHostWithTopshelf.exe start
+停止：WindowsServiceGenericHostWithTopshelf.exe stop
+卸载：WindowsServiceGenericHostWithTopshelf.exe uninstall
